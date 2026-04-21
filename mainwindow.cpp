ba0240b4a12f43583dd81859394b13e4b56357b2
Out Law <luuminhthoat@gmail.com>
2026-04-21 20:52:26 +0700

    Load file .OBJ, .mtl with texture

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,10 @@
 #include <QFileDialog>
 #include <QMessageBox>
 #include <QDebug>
+#include <QFileInfo>
+#include <vtkOBJImporter.h>
 #include <vtkOBJReader.h>
+#include <vtkActorCollection.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkActor.h>
 #include <vtkRenderer.h>
@@ -18,6 +21,7 @@
 #include <vtkUnsignedCharArray.h>
 #include <vtkPointData.h>
 #include <vtkProperty.h>
+#include <vtkLight.h>
 #include "PanStyle.h"
 
 MainWindow::MainWindow(QWidget* parent)
@@ -34,17 +38,95 @@ MainWindow::MainWindow(QWidget* parent)
     renderer->SetBackground(0.1, 0.2, 0.4);
     vtkWidget->renderWindow()->AddRenderer(renderer);
 
-    // Load OBJ mặc định
-    vtkNew<vtkOBJReader> reader;
-    reader->SetFileName("C:/Users/ADMIN/Documents/3D-Reconstruction/3DModels/Audi_R8_2017.obj");
-    reader->Update();
+    // Thêm đèn headlight để chiếu sáng từ camera
+    vtkSmartPointer<vtkLight> headlight = vtkSmartPointer<vtkLight>::New();
+    headlight->SetLightTypeToHeadlight();
+    headlight->SetIntensity(1.5);
+    renderer->AddLight(headlight);
 
-    vtkNew<vtkPolyDataMapper> mapper;
-    mapper->SetInputConnection(reader->GetOutputPort());
+    // Thêm các đèn phụ
+    vtkSmartPointer<vtkLight> light1 = vtkSmartPointer<vtkLight>::New();
+    light1->SetLightTypeToSceneLight();
+    light1->SetPosition(2.0, 3.0, 4.0);
+    light1->SetIntensity(1.0);
+    renderer->AddLight(light1);
 
-    objActor = vtkSmartPointer<vtkActor>::New();
-    objActor->SetMapper(mapper);
-    renderer->AddActor(objActor);
+    vtkSmartPointer<vtkLight> light2 = vtkSmartPointer<vtkLight>::New();
+    light2->SetLightTypeToSceneLight();
+    light2->SetPosition(-2.0, 1.0, 3.0);
+    light2->SetIntensity(0.8);
+    renderer->AddLight(light2);
+
+    // Load OBJ + MTL
+    QString objPath = "C:/Users/ADMIN/Documents/3D-Reconstruction/3DModels/85-cottage_obj/cottage_obj.obj";
+    QString mtlPath = "C:/Users/ADMIN/Documents/3D-Reconstruction/3DModels/85-cottage_obj/cottage_obj.mtl";
+
+    QFileInfo objFile(objPath);
+    QFileInfo mtlFile(mtlPath);
+    qDebug() << "OBJ exists:" << objFile.exists();
+    qDebug() << "MTL exists:" << mtlFile.exists();
+
+    vtkNew<vtkOBJImporter> importer;
+    importer->SetFileName(objPath.toStdString().c_str());
+    if (mtlFile.exists())
+        importer->SetFileNameMTL(mtlPath.toStdString().c_str());
+    importer->Update();
+
+    vtkRenderer* importerRenderer = importer->GetRenderer();
+    if (importerRenderer) {
+        vtkActorCollection* actors = importerRenderer->GetActors();
+        actors->InitTraversal();
+        vtkActor* actor;
+        int actorCount = 0;
+        while ((actor = actors->GetNextActor())) {
+            // In số polygons và màu gốc từ material
+            vtkPolyData* polyData = vtkPolyData::SafeDownCast(actor->GetMapper()->GetInput());
+            if (polyData) {
+                vtkIdType numPolys = polyData->GetNumberOfPolys();
+                qDebug() << "Actor" << actorCount << "has" << numPolys << "polygons";
+            } else {
+                qDebug() << "Actor" << actorCount << "has no poly data";
+            }
+
+            // Lấy màu đã được importer đọc từ MTL
+            double* color = actor->GetProperty()->GetColor();
+            qDebug() << "Material color from MTL (R,G,B):" << color[0] << color[1] << color[2];
+
+            // Bật lighting để màu hiển thị đúng (không ghi đè)
+            actor->GetProperty()->SetLighting(true);
+            actor->GetProperty()->SetInterpolationToPhong();
+            actor->GetProperty()->SetAmbient(0.3);
+            actor->GetProperty()->SetDiffuse(0.8);
+
+            renderer->AddActor(actor);
+            actorCount++;
+        }
+        qDebug() << "Actors added from OBJ importer:" << actorCount;
+        if (actorCount == 0) {
+            // fallback...
+            vtkNew<vtkOBJReader> reader;
+            reader->SetFileName(objPath.toStdString().c_str());
+            reader->Update();
+            vtkNew<vtkPolyDataMapper> mapper;
+            mapper->SetInputConnection(reader->GetOutputPort());
+            objActor = vtkSmartPointer<vtkActor>::New();
+            objActor->SetMapper(mapper);
+            objActor->GetProperty()->SetColor(0.7, 0.7, 0.7);
+            renderer->AddActor(objActor);
+        }
+    } else {
+        // fallback...
+        vtkNew<vtkOBJReader> reader;
+        reader->SetFileName(objPath.toStdString().c_str());
+        reader->Update();
+        vtkNew<vtkPolyDataMapper> mapper;
+        mapper->SetInputConnection(reader->GetOutputPort());
+        objActor = vtkSmartPointer<vtkActor>::New();
+        objActor->SetMapper(mapper);
+        objActor->GetProperty()->SetColor(0.7, 0.7, 0.7);
+        renderer->AddActor(objActor);
+    }
+
     renderer->ResetCamera();
 
     // Interactor style
@@ -111,10 +193,8 @@ void MainWindow::onShowPointCloud()
 
     qDebug() << "Showing point cloud: points=" << pts.size() << ", colors=" << colors.size();
 
-    // Xóa cloud cũ nếu có
     onClearPointCloud();
 
-    // Tạo vtkPoints và vtkPolyData
     vtkNew<vtkPoints> vtkPoints;
     vtkNew<vtkCellArray> vertices;
     vtkNew<vtkUnsignedCharArray> vtkColors;
@@ -126,10 +206,8 @@ void MainWindow::onShowPointCloud()
         vertices->InsertNextCell(1);
         vertices->InsertCellPoint(i);
         if (i < colors.size()) {
-            // colors[i] là cv::Vec3b (B,G,R). VTK cần (R,G,B)
             vtkColors->InsertNextTuple3(colors[i][2], colors[i][1], colors[i][0]);
         } else {
-            // Mặc định màu trắng
             vtkColors->InsertNextTuple3(255, 255, 255);
         }
     }
@@ -139,7 +217,6 @@ void MainWindow::onShowPointCloud()
     polyData->SetVerts(vertices);
     polyData->GetPointData()->SetScalars(vtkColors);
 
-    // Sử dụng vtkVertexGlyphFilter để tạo các điểm
     vtkNew<vtkVertexGlyphFilter> glyphFilter;
     glyphFilter->SetInputData(polyData);
     glyphFilter->Update();
@@ -149,7 +226,6 @@ void MainWindow::onShowPointCloud()
 
     cloudActor = vtkSmartPointer<vtkActor>::New();
     cloudActor->SetMapper(cloudMapper);
-    // Tăng kích thước điểm để nhìn rõ màu
     cloudActor->GetProperty()->SetPointSize(3);
 
     renderer->AddActor(cloudActor);
