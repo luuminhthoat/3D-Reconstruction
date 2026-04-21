81ad6332c6163ed6edb184be12f4c2be37ee0b84
Out Law <luuminhthoat@gmail.com>
2026-04-21 09:47:27 +0700

    Demo use lib VTK và 3d wheel, rotate

---
+++ mainwindow.cpp
@@ -0,0 +1,41 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QVBoxLayout>
+
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // Tạo QVTKOpenGLNativeWidget làm central widget
+    QVTKOpenGLNativeWidget* vtkWidget = new QVTKOpenGLNativeWidget(this);
+    setCentralWidget(vtkWidget);
+
+    // Tạo nguồn hình học: hình trụ
+    vtkNew<vtkCylinderSource> cylinder;
+    cylinder->SetHeight(3.0);
+    cylinder->SetRadius(1.0);
+    cylinder->SetResolution(32);
+
+    // Mapper
+    vtkNew<vtkPolyDataMapper> mapper;
+    mapper->SetInputConnection(cylinder->GetOutputPort());
+
+    // Actor
+    vtkNew<vtkActor> actor;
+    actor->SetMapper(mapper);
+
+    // Renderer
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor(actor);
+    renderer->SetBackground(0.1, 0.2, 0.4);
+
+    // Gán renderer vào VTK widget
+    vtkWidget->renderWindow()->AddRenderer(renderer);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QVTKOpenGLNativeWidget.h>
+#include <vtkCylinderSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow* ui;
+};
+
+#endif
